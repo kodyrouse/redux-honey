4781398c96e09023ddd1d89982e96340e44b1cd3
Kody Rouse <59030518+slingshot-kodyrouse@users.noreply.github.com>
2020-09-17 22:53:43 -0400

    removed ability to set default arrayOf values

--- src/arrayOf/index.js
+++ src/arrayOf/index.js
@@ -16,12 +16,12 @@ export default (...options) =>  {
 
     // This adds the given default value as part of the state map to return
     // This given set array is validated in createTypeMapsForStates
-    if (options.length >= 2) {
-      if (Array.isArray(options[1]))
-        returnObj.__initialValue = options[1];
-      else
-        log.warn(`The key "${key}" for addHoney("${stateKey}") is using arrayOf() & is passing an initial value, however the value given is not an array.`);
-    }
+    // if (options.length >= 2) {
+    //   if (Array.isArray(options[1]))
+    //     returnObj.__initialValue = options[1];
+    //   else
+    //     log.warn(`The key "${key}" for addHoney("${stateKey}") is using arrayOf() & is passing an initial value, however the value given is not an array.`);
+    // }
 
     return returnObj;
   }
